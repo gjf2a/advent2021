e0bc1db85058c8f280c9eb70353ce557bd8879c5
gjf2a <ferrer@hendrix.edu>
2021-12-01 12:55:19 -0600

    Consolidated the day1 code into a single program.

---
+++ src/bin/day1.rs
@@ -0,0 +1,25 @@
+use std::io;
+use advent_code_lib::file2nums;
+use std::env;
+
+fn window_total(depths: &Vec<isize>, i: usize, window_width: usize) -> isize {
+    (i..i+window_width).map(|j| depths[j]).sum()
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: day1 filename window_width")
+    } else {
+        let depths = file2nums(format!("in/{}", args[1]).as_str())?;
+        let mut count = 0;
+        let window_width = args[2].parse::<usize>().unwrap();
+        for i in 0..depths.len() - window_width {
+            if window_total(&depths, i, window_width) < window_total(&depths, i + 1, window_width) {
+                count += 1;
+            }
+        }
+        println!("{}", count);
+    }
+    Ok(())
+}
