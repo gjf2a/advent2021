cee4a6df3421bae47bd25c98c732665d5c217db8
gjf2a <ferrer@hendrix.edu>
2021-12-03 05:15:22 -0600

    Day 3 part 2 works.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,19 +6,58 @@ fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: day3 filename");
+        Ok(())
     } else {
         let binary_nums: Vec<BitArray> = all_lines(args[1].as_str())?.map(|s| s.parse().unwrap()).collect();
-        let width = binary_nums[0].len();
-        let majority_min = binary_nums.len() / 2 + 1;
-        let gamma: BitArray = (0..width)
-            .map(|i| binary_nums.iter().filter(|b| b.is_set(i)).count() >= majority_min)
-            .collect();
-        let epsilon = !&gamma;
-        let gamma_rate = u64::try_from(&gamma).unwrap();
-        let epsilon_rate = u64::try_from(&epsilon).unwrap();
-        println!("gamma: {} ({})", gamma, gamma_rate);
-        println!("epsilon: {} ({})", epsilon, epsilon_rate);
-        println!("product: {}", gamma_rate * epsilon_rate);
+        part1(&binary_nums)?;
+        part2(&binary_nums)
     }
+}
+
+fn part1(binary_nums: &Vec<BitArray>) -> io::Result<()> {
+    let gamma= find_gamma(binary_nums);
+    show_results(&gamma, "gamma", &!&gamma, "epsilon")
+}
+
+fn find_gamma(binary_nums: &Vec<BitArray>) -> BitArray {
+    let width = binary_nums[0].len();
+    (0..width)
+        .map(|i| one_most_common_at(i, binary_nums))
+        .collect()
+}
+
+fn one_most_common_at(index: u64, binary_nums: &Vec<BitArray>) -> bool {
+    let one_count = binary_nums.iter().filter(|b| b.is_set(index)).count();
+    one_count >= binary_nums.len() - one_count
+}
+
+fn part2(binary_nums: &Vec<BitArray>) -> io::Result<()> {
+    let oxygen_generator_rating = filter_using(false, binary_nums);
+    let co2_scrubber_rating = filter_using(true, binary_nums);
+    show_results(&oxygen_generator_rating, "oxygen generator rating", &co2_scrubber_rating, "CO2 scrubber rating")
+}
+
+fn filter_using(least_common: bool, binary_nums: &Vec<BitArray>) -> BitArray {
+    let mut binary_nums = binary_nums.clone();
+    let mut index = binary_nums[0].len();
+    while binary_nums.len() > 1 {
+        index -= 1;
+        let target = one_most_common_at(index, &binary_nums) ^ least_common;
+        binary_nums = binary_nums.iter()
+            .filter(|b| b.is_set(index) == target)
+            .map(|b| b.clone())
+            .collect();
+    }
+    binary_nums[0].clone()
+}
+
+fn show_results(num1: &BitArray, num1name: &str, num2: &BitArray, num2name: &str) -> io::Result<()> {
+    let mut product = 1;
+    for (num, name) in [(num1, num1name), (num2, num2name)] {
+        let rate = u64::try_from(num)?;
+        println!("{}: {} ({})", name, num, rate);
+        product *= rate;
+    }
+    println!("product: {}", product);
     Ok(())
 }
