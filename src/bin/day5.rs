fddacbbe5e65d750f91b390ae07083825e27aef8
gjf2a <ferrer@hendrix.edu>
2021-12-05 06:03:36 -0600

    Day 5 part 2 works!

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,4 +1,4 @@
-use std::{env, io};
+use std::{env, io, iter};
 use std::cmp::{max, min};
 use std::str::FromStr;
 use advent_code_lib::{all_lines, Position};
@@ -6,9 +6,11 @@ use hash_histogram::HashHistogram;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: day5.txt filename");
+    if args.len() < 3 {
+        println!("Usage: day5.txt filename (1|2) [show]");
     } else {
+        let with_diagonals = args[2] == "2";
+        let show = args.len() == 4;
         let segments = all_lines(args[1].as_str())?
             .map(|line| line.parse::<LineSegment>())
             .collect::<Result<Vec<LineSegment>, io::Error>>()?;
@@ -16,18 +18,15 @@ fn main() -> io::Result<()> {
         let (width, height) = (max_x + 1, max_y + 1);
         let mut counts = HashHistogram::new();
         for segment in segments.iter() {
-            for p in segment.horizontals() {
-                counts.bump(&p);
-            }
-            for p in segment.verticals() {
+            for p in segment.points(with_diagonals) {
                 counts.bump(&p);
             }
         }
-        print_diagram(&counts, width, height);
+        if show {print_diagram(&counts, width, height);}
         let score_part_1 = counts.iter()
             .filter(|(_, count)| **count >= 2)
             .count();
-        println!("Part 1 score: {}", score_part_1);
+        println!("Score: {}", score_part_1);
     }
     Ok(())
 }
@@ -61,30 +60,60 @@ fn max_from(acc: (usize, usize), seg: &LineSegment) -> (usize, usize) {
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-struct LineSegment {
+pub struct LineSegment {
     start: Position,
     end: Position
 }
 
 impl LineSegment {
-    fn horizontals(&self) -> impl Iterator<Item=Position> + '_ {
+    // Returning an Iterator: https://stackoverflow.com/questions/27535289/what-is-the-correct-way-to-return-an-iterator-or-any-other-trait
+    pub fn points<'a>(&'a self, with_diagonals: bool) -> Box<dyn Iterator<Item = Position> + 'a> {
         if self.start.row == self.end.row {
             let start = min(self.start.col, self.end.col);
             let end = max(self.start.col, self.end.col);
-            start..=end
-        } else {
-            0..=-1
-        }.map(|x| Position::from((x, self.start.row)))
-    }
-
-    fn verticals(&self) -> impl Iterator<Item=Position> + '_ {
-        if self.start.col == self.end.col {
+            Box::new((start..=end).map(|x| Position::from((x, self.start.row))))
+        } else if self.start.col == self.end.col {
             let start = min(self.start.row, self.end.row);
             let end = max(self.start.row, self.end.row);
-            start..=end
+            Box::new((start..=end).map(|y| Position::from((self.start.col, y))))
+        } else if with_diagonals && (self.start.col - self.end.col).abs() == (self.start.row - self.end.row).abs() {
+            Box::new(DiagonalIterator::from(self.start, self.end))
         } else {
-            0..=-1
-        }.map(|y| Position::from((self.start.col, y)))
+            Box::new(iter::empty::<Position>())
+        }
+    }
+}
+
+fn find_offset(start: isize, end: isize) -> isize {
+    if start < end {1} else {-1}
+}
+
+struct DiagonalIterator {
+    d: Position,
+    current: Position,
+    last: Position,
+    active: bool
+}
+
+impl DiagonalIterator {
+    fn from(start: Position, end: Position) -> Self {
+        DiagonalIterator {d: Position::from((find_offset(start.col, end.col), find_offset(start.row, end.row))),
+            current: start, last: end, active: true }
+    }
+}
+
+impl Iterator for DiagonalIterator {
+    type Item = Position;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.active {
+            let result = self.current;
+            self.active = self.current != self.last;
+            self.current += self.d;
+            Some(result)
+        } else {
+            None
+        }
     }
 }
 
@@ -98,5 +127,15 @@ impl FromStr for LineSegment {
             .collect::<Result<Vec<Position>, io::Error>>()
             .unwrap();
         Ok(LineSegment { start: parsed[0], end: parsed[1]})
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_diagonal() {
+        println!("{:?}", LineSegment::from_str("1,1 -> 3,3").unwrap().points(true).collect::<Vec<Position>>());
     }
 }
