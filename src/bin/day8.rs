07cc1c06e7aefde57b0c11d76fc2e95a37fe7d1a
gjf2a <ferrer@hendrix.edu>
2021-12-08 16:06:19 -0600

    Day 8: Rearranged code.

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -20,6 +20,19 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn solve_part_1(entries: &Vec<DeviceEntry>) -> usize {
+    let easy_lengths: HashSet<usize> = find_easy_lengths(&PATTERN_FOR);
+    entries.iter()
+        .map(|entry| entry.outputs.iter()
+            .filter(|output| easy_lengths.contains(&output.len()))
+            .count())
+        .sum()
+}
+
+fn solve_part_2(entries: &Vec<DeviceEntry>) -> usize {
+    entries.iter().map(|entry| entry.output_value()).sum()
+}
+
 #[derive(Debug)]
 struct DeviceEntry {
     inputs: Vec<String>,
@@ -34,34 +47,6 @@ impl DeviceEntry {
         DeviceEntry {inputs, outputs}
     }
 
-    fn find_mapping(&self) -> HashMap<char, char> {
-        let mut result = HashMap::new();
-        let lengths2charsets = char_sets_by_lengths(&self.inputs);
-        sanity_check(&lengths2charsets);
-
-        let one = singleton(&lengths2charsets, 2);
-        let mut seven = singleton(&lengths2charsets, 3).clone();
-        seven.retain(|c| !one.contains(c));
-        result.insert(retrieve_singleton(&seven), 'a');
-
-        for (c, lengths) in [
-            ('g', vec![5, 6]),
-            ('d', vec![5]),
-            ('f', vec![2, 6]),
-            ('c', vec![2]),
-            ('b', vec![4]),
-            ('e', vec![7])
-        ] {
-            let found = finalize(&result, &mut big_intersection(big_chain(&lengths, &lengths2charsets)));
-            result.insert(found, c);
-        }
-        result
-    }
-
-    fn output_digits<'a>(&'a self, mapping: &'a HashMap<char, char>) -> impl Iterator<Item=usize> + 'a {
-        self.outputs.iter().map(|output| decode(output.as_str(), mapping))
-    }
-
     fn output_value(&self) -> usize {
         let mut total = 0;
         for digit in self.output_digits(&self.find_mapping()) {
@@ -70,22 +55,59 @@ impl DeviceEntry {
         }
         total
     }
+
+    fn find_mapping(&self) -> HashMap<char, char> {
+        let mut result = HashMap::new();
+        let lengths2charsets = char_sets_by_lengths(&self.inputs);
+        sanity_check(&lengths2charsets);
+
+        result.insert(find_a(&lengths2charsets), 'a');
+        for (c, lengths) in [
+            ('g', vec![5, 6]),
+            ('d', vec![5]),
+            ('f', vec![2, 6]),
+            ('c', vec![2]),
+            ('b', vec![4]),
+            ('e', vec![7])
+        ] {
+            let found = finalize(&result, &mut intersection(set_chain(&lengths, &lengths2charsets)));
+            result.insert(found, c);
+        }
+        result
+    }
+
+    fn output_digits<'a>(&'a self, mapping: &'a HashMap<char, char>) -> impl Iterator<Item=usize> + 'a {
+        self.outputs.iter().map(|output| decode(output.as_str(), mapping))
+    }
+}
+
+fn sanity_check(lengths2charsets: &HashMap<usize, Vec<HashSet<char>>>) {
+    for (length, count) in [(2, 1), (3, 1), (4, 1), (5, 3), (6, 3), (7, 1)] {
+        assert_eq!(lengths2charsets.get(&length).unwrap().len(), count);
+    }
+}
+
+fn find_a(lengths2charsets: &HashMap<usize, Vec<HashSet<char>>>) -> char {
+    let one = singleton(lengths2charsets, 2);
+    let mut seven = singleton(lengths2charsets, 3).clone();
+    seven.retain(|c| !one.contains(c));
+    retrieve_singleton(&seven)
 }
 
 fn decode(output: &str, mapping: &HashMap<char, char>) -> usize {
     let mut chars: Vec<char> = output.chars().map(|c| mapping.get(&c).unwrap()).copied().collect();
     chars.sort();
     let decoded: String = chars.iter().collect();
-    PATTERN_FOR.iter().enumerate().find(|(i, s)| decoded.as_str() == **s).map(|(i, _)| i).unwrap()
+    PATTERN_FOR.iter().enumerate().find(|(_, s)| decoded.as_str() == **s).map(|(i, _)| i).unwrap()
 }
 
-fn big_chain<'a>(lengths: &'a [usize], lengths2charsets: &'a HashMap<usize, Vec<HashSet<char>>>)
-    -> impl Iterator<Item=&'a HashSet<char>> {
+fn set_chain<'a>(lengths: &'a [usize], lengths2charsets: &'a HashMap<usize, Vec<HashSet<char>>>)
+                 -> impl Iterator<Item=&'a HashSet<char>> {
     lengths.iter()
         .flat_map(|length| lengths2charsets.get(length).unwrap().iter())
 }
 
-fn big_intersection<'a, I: Iterator<Item=&'a HashSet<char>>>(mut sets: I) -> HashSet<char> {
+fn intersection<'a, I: Iterator<Item=&'a HashSet<char>>>(mut sets: I) -> HashSet<char> {
     let mut result = sets.next().unwrap().clone();
     for set in sets {
         result.retain(|c| set.contains(c));
@@ -118,23 +140,15 @@ fn char_sets_by_lengths(inputs: &Vec<String>) -> HashMap<usize, Vec<HashSet<char
         .collect()
 }
 
-fn sanity_check(lengths2charsets: &HashMap<usize, Vec<HashSet<char>>>) {
-    for (length, count) in [(2, 1), (3, 1), (4, 1), (5, 3), (6, 3), (7, 1)] {
-        assert_eq!(lengths2charsets.get(&length).unwrap().len(), count);
-    }
-}
-
 fn snag_put(part: &str) -> Vec<String> {
     part.split_whitespace().map(|s| s.to_owned()).collect()
 }
 
-fn solve_part_1(entries: &Vec<DeviceEntry>) -> usize {
-    let easy_lengths: HashSet<usize> = find_easy_lengths(&PATTERN_FOR);
-    entries.iter()
-        .map(|entry| entry.outputs.iter()
-            .filter(|output| easy_lengths.contains(&output.len()))
-            .count())
-        .sum()
+fn find_easy_lengths(strs: &[&str]) -> HashSet<usize> {
+    by_lengths(strs.iter().copied()).iter()
+        .filter(|(_, patterns)| patterns.len() == 1)
+        .map(|(len, _)| *len)
+        .collect()
 }
 
 fn by_lengths<'a, I: Iterator<Item=&'a str>>(strs: I) -> HashMap<usize, Vec<String>> {
@@ -146,15 +160,4 @@ fn by_lengths<'a, I: Iterator<Item=&'a str>>(strs: I) -> HashMap<usize, Vec<Stri
         }
     }
     result
-}
-
-fn find_easy_lengths(strs: &[&str]) -> HashSet<usize> {
-    by_lengths(strs.iter().copied()).iter()
-        .filter(|(_, patterns)| patterns.len() == 1)
-        .map(|(len, _)| *len)
-        .collect()
-}
-
-fn solve_part_2(entries: &Vec<DeviceEntry>) -> usize {
-    entries.iter().map(|entry| entry.output_value()).sum()
 }
