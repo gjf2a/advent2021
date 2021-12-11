13b44f6b5e2d7aa548a7bb49869b48e5e9870023
gjf2a <ferrer@hendrix.edu>
2021-12-11 06:13:22 -0600

    Day 11, part 1 works!

---
+++ src/bin/day11.rs
@@ -0,0 +1,107 @@
+use std::cmp::max;
+use std::collections::{HashMap, VecDeque};
+use std::fmt::{Display, Formatter};
+use std::io;
+use advent_code_lib::{all_lines, generic_main, Position, RowMajorPositionIterator};
+use bare_metal_modulo::{MNum, ModNumC};
+
+fn main() -> io::Result<()> {
+    generic_main("day9", &[], &["-show:num_steps"], |args| {
+        let octopi = DumboOctopi::new(args[1].as_str())?;
+        match args.iter().find(|arg| arg.starts_with("-show")) {
+            None => {
+                println!("Part 1 score: {}", part_1(octopi.clone()));
+            }
+            Some(show_step) => {
+                let steps: usize = show_step.split(':').nth(1).unwrap().parse().unwrap();
+                show_steps(octopi, steps);
+            }
+        }
+        Ok(())
+    })
+}
+
+fn part_1(octopi: DumboOctopi) -> usize {
+    octopi.take(100).sum()
+}
+
+fn show_steps(mut octopi: DumboOctopi, steps: usize) {
+    println!("Before any steps:");
+    println!("{}", octopi);
+    println!();
+
+    for step in 1..=steps {
+        let flashes = octopi.next().unwrap();
+        println!("After step {} ({} flashes):", step, flashes);
+        println!("{}", octopi);
+        println!();
+    }
+
+}
+
+#[derive(Clone, Debug)]
+struct DumboOctopi {
+    energies: HashMap<Position, ModNumC<u32, 10> >,
+    width: usize,
+    height: usize
+}
+
+impl DumboOctopi {
+    fn new(filename: &str) -> io::Result<DumboOctopi> {
+        let mut width = 0;
+        let mut height = 0;
+        let mut energies = HashMap::new();
+        all_lines(filename)?.enumerate()
+            .for_each(|(row, row_chars)| row_chars.chars().enumerate()
+                .for_each(|(col, energy)| {
+                    energies.insert(Position::from((col as isize, row as isize)),
+                                    ModNumC::new(energy.to_digit(10).unwrap()));
+                    width = max(width, col + 1);
+                    height = max(height, row + 1);
+                }));
+        Ok(DumboOctopi {energies, width, height})
+    }
+}
+
+impl Display for DumboOctopi {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for p in RowMajorPositionIterator::new(self.width, self.height) {
+            if p.col == 0 && p.row > 0 {writeln!(f)?;}
+            write!(f, "{}", self.energies.get(&p).unwrap().a())?
+        }
+        Ok(())
+    }
+}
+
+impl Iterator for DumboOctopi {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for (_, energy) in self.energies.iter_mut() {
+            *energy += 1;
+        }
+        let mut queue: VecDeque<Position> = self.energies.iter()
+            .filter(|(_, energy)| **energy == 0)
+            .map(|(p, _)| *p)
+            .collect();
+        let mut flashes = queue.len();
+        loop {
+            match queue.pop_front() {
+                None => return Some(flashes),
+                Some(flasher) => {
+                    for neighbor in flasher.neighbors() {
+                        if let Some(neighbor_energy) = self.energies.get_mut(&neighbor) {
+                            if neighbor_energy != &0 {
+                                *neighbor_energy += 1;
+                                if neighbor_energy == &0 {
+                                    queue.push_front(neighbor);
+                                    flashes += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
