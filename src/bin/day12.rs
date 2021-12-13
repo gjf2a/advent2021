881b18d34ac4b8d4644e585362d033d104cca5cd
gjf2a <ferrer@hendrix.edu>
2021-12-12 20:13:33 -0600

    Day 12: Part 1 optimized and works!

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,4 +1,5 @@
-use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use std::collections::{BTreeMap, BTreeSet, HashSet, VecDeque};
+use std::fmt::{Display, Formatter};
 use std::io;
 use advent_code_lib::{AdjacencySets, all_lines, Arena, generic_main, search, SearchQueue};
 use common_macros::b_tree_set;
@@ -18,6 +19,7 @@ fn main() -> io::Result<()> {
         let table = PathTable::new(&graph);
         if let Some(arg) = args.get(2) {
             if arg.as_str() == SHOW_PATH_ARG {
+                println!("{}", table);
                 let mut unique = BTreeSet::new();
                 for path in table.all_paths_to(END) {
                     unique.insert(format!("{:?}", path));
@@ -53,22 +55,26 @@ impl PathTable {
         let mut table: Vec<BTreeMap<String,BTreeSet<usize>>> = Vec::new();
         let mut arena = Arena::new();
         let mut open_list = VecDeque::new();
+        let mut visited = HashSet::new();
         open_list.push_back((0, START.to_string(), None));
         search(open_list, |(level, node, parent): &(usize, String, Option<String>), q| {
-            let parent_paths = parent.clone().map(|p| table[*level - 1].get(p.as_str()).unwrap());
-            let mut paths_to = PathTable::make_paths_for(node.as_str(), &parent_paths, &mut arena);
-            if paths_to.len() > 0 {
-                if table.len() == *level {
-                    table.push(BTreeMap::new());
-                }
-                match table[*level].get_mut(node) {
-                    None => {table[*level].insert(node.clone(), paths_to);}
-                    Some(paths) => {paths.append(&mut paths_to);}
-                }
+            if !visited.contains(&(*level, node.clone(), parent.clone())) {
+                visited.insert((*level, node.clone(), parent.clone()));
+                let parent_paths = parent.clone().map(|p| table[*level - 1].get(p.as_str()).unwrap());
+                let mut paths_to = PathTable::make_paths_for(node.as_str(), &parent_paths, &mut arena);
+                if paths_to.len() > 0 {
+                    if table.len() == *level {
+                        table.push(BTreeMap::new());
+                    }
+                    match table[*level].get_mut(node) {
+                        None => { table[*level].insert(node.clone(), paths_to); }
+                        Some(paths) => { paths.append(&mut paths_to); }
+                    }
 
-                if node.as_str() != END {
-                    for neighbor in graph.neighbors_of(node.as_str()).unwrap() {
-                        q.enqueue(&(level + 1, neighbor.clone(), Some(node.clone())));
+                    if node.as_str() != END {
+                        for neighbor in graph.neighbors_of(node.as_str()).unwrap() {
+                            q.enqueue(&(level + 1, neighbor.clone(), Some(node.clone())));
+                        }
                     }
                 }
             }
@@ -99,16 +105,36 @@ impl PathTable {
         for row in self.table.iter() {
             if let Some(row) = row.get(node) {
                 for path_end in row.iter() {
-                    let mut path = self.arena.get(*path_end).iter(&self.arena).collect::<Vec<_>>();
-                    path.reverse();
-                    result.push(path.iter().map(|s| (*s).clone()).collect());
+                    result.push(self.path_at_addr(*path_end));
                 }
             }
         }
         result
     }
 
+    fn path_at_addr(&self, addr: usize) -> Vec<String> {
+        let mut path = self.arena.get(addr).iter(&self.arena).collect::<Vec<_>>();
+        path.reverse();
+        path.iter().map(|s| (*s).clone()).collect()
+    }
+
     fn total_path_count_to(&self, node: &str) -> usize {
         self.all_paths_to(node).len()
+    }
+}
+
+impl Display for PathTable {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for (row_num, row) in self.table.iter().enumerate() {
+            writeln!(f, "Row {}", row_num)?;
+            for (node, parents) in row.iter() {
+                writeln!(f, "Node: {}", node)?;
+                for (id, path) in parents.iter().map(|id| (id, self.path_at_addr(*id))) {
+                    writeln!(f, "({}) {:?}", id, path)?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
