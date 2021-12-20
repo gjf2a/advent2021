cb868fe43d635b9472a6616fb88fb7b7dad09de1
gjf2a <ferrer@hendrix.edu>
2021-12-20 06:41:40 -0600

    Day 20: After a rewrite, still works on Part 1 example, but not on my puzzle input.
    5467 is still too low and 5685 is too high.

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,12 +1,14 @@
 use std::cmp::{max, min};
-use std::collections::HashSet;
-use std::{io, mem};
+use std::collections::HashMap;
+use std::{io, iter, mem};
 use std::fmt::{Display, Formatter};
-use advent_code_lib::{advent_main, all_lines, Position, RowMajorPositionIterator};
+use advent_code_lib::{advent_main, all_lines, OffsetRowMajorPositionIterator, Position, RowMajorPositionIterator};
 use bits::BitArray;
 
 const ON:  char = '#';
 const OFF: char = '.';
+const PART_1_ITER: usize = 2;
+const BORDER: isize = 1;
 
 fn main() -> io::Result<()> {
     advent_main(&[], &[], |args| {
@@ -15,28 +17,38 @@ fn main() -> io::Result<()> {
         lines.next();
         let image = read_image(&mut lines);
         let mut enhancer = ImageEnhancer::new(image, algorithm);
-        for i in 0..=2 {
+        for i in 0..=PART_1_ITER {
             let image = enhancer.next().unwrap();
-            //println!("{}", image);
-            println!("{}: {}", i, image.num_lit());
+            println!("After step {}", i);
+            println!("{}", image);
+            println!("{:?}", image.num_lit());
         }
         Ok(())
     })
 }
 
+fn code2pixel(c: char) -> bool {
+    c == ON
+}
+
+fn pixel2code(pixel: bool) -> char {
+    if pixel {ON} else {OFF}
+}
+
 fn read_enhancement_algorithm(line: &str) -> BitArray {
     let mut result = BitArray::new();
     for code in line.chars() {
-        result.add(code == ON);
+        result.add(code2pixel(code));
     }
+    assert_eq!(result.len(), 512);
     result
 }
 
 fn read_image<I: Iterator<Item=String>>(lines: &mut I) -> Image {
     let mut image = Image::new();
     for (row, line) in lines.enumerate() {
-        for (col, bit) in line.chars().enumerate() {
-            image.set(col as isize, row as isize, bit == ON);
+        for (col, code) in line.chars().enumerate() {
+            image.set(col as isize, row as isize, code2pixel(code));
         }
     }
     image
@@ -44,7 +56,8 @@ fn read_image<I: Iterator<Item=String>>(lines: &mut I) -> Image {
 
 #[derive(Clone, Eq, PartialEq, Debug)]
 struct Image {
-    bits: HashSet<Position>,
+    bits: HashMap<Position,bool>,
+    infinite_expanse: bool,
     min_row: isize,
     min_col: isize,
     max_row: isize,
@@ -52,39 +65,27 @@ struct Image {
 }
 
 impl Image {
-    fn new() -> Self {Image {bits: HashSet::new(), min_row: 0, min_col: 0, max_row: 0, max_col: 0}}
+    fn new() -> Self {Image {bits: HashMap::new(), infinite_expanse: false, min_row: 0, min_col: 0, max_row: 0, max_col: 0}}
 
-    fn lowest_pos(&self) -> Position {
-        Position::from((self.min_col, self.min_row))
-    }
-
-    fn next_pos(&self, p: Position) -> Option<Position> {
-        let mut updated = Position::from((p.col + 1, p.row));
-        if updated.col > self.max_col {
-            updated.col = self.min_col;
-            updated.row += 1;
-            if updated.row > self.max_row {
-                return None
-            }
-        }
-        Some(updated)
+    fn width(&self) -> usize {
+        (self.max_col - self.min_col + 1) as usize
     }
 
     fn set(&mut self, col: isize, row: isize, value: bool) {
         let p = Position::from((col, row));
-        if value {
-            self.bits.insert(p);
-        } else {
-            self.bits.remove(&p);
-        }
-        self.min_row = min(row - 1, self.min_row);
-        self.min_col = min(col - 1, self.min_col);
-        self.max_row = max(row + 1, self.max_row);
-        self.max_col = max(col + 1, self.max_col);
+        self.bits.insert(p, value);
+        self.min_row = min(row, self.min_row);
+        self.min_col = min(col, self.min_col);
+        self.max_row = max(row, self.max_row);
+        self.max_col = max(col, self.max_col);
     }
 
     fn on(&self, p: Position) -> bool {
-        self.bits.contains(&p)
+        self.bits.get(&p).map_or(self.infinite_expanse, |b| *b)
+    }
+
+    fn encoded(&self, p: Position) -> char {
+        pixel2code(self.on(p))
     }
 
     fn neighborhood(&self, p: Position) -> u64 {
@@ -100,25 +101,37 @@ impl Image {
         result
     }
 
-    fn num_lit(&self) -> usize {
-        self.bits.len()
+    fn num_lit(&self) -> Option<usize> {
+        if self.infinite_expanse {
+            None
+        } else {
+            Some(self.bits.values().filter(|v| **v).count())
+        }
+    }
+
+    fn infinite_row(&self) -> String {
+        iter::repeat(self.infinite_char()).take(self.width() + 2).collect()
+    }
+
+    fn infinite_char(&self) -> char {
+        pixel2code(self.infinite_expanse)
     }
 }
 
 impl Display for Image {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let mut pixel = self.lowest_pos();
-        loop {
-            write!(f, "{}", if self.on(pixel) {ON} else {OFF})?;
-            match self.next_pos(pixel) {
-                None => return Ok(()),
-                Some(next) => {
-                    if next.col == self.min_col {writeln!(f)?;}
-                    pixel = next;
+        write!(f, "{}\n{}", self.infinite_row(), self.infinite_char())?;
+        for pixel in OffsetRowMajorPositionIterator::new(self.min_col, self.min_row, self.max_col, self.max_row) {
+            write!(f, "{}", self.encoded(pixel))?;
+            if pixel.col == self.max_col {
+                writeln!(f, "{}", self.infinite_char())?;
+                if pixel.row < self.max_row {
+                    write!(f, "{}", self.infinite_char())?;
                 }
             }
-
         }
+        writeln!(f, "{}", self.infinite_row())?;
+        Ok(())
     }
 }
 
@@ -138,13 +151,11 @@ impl Iterator for ImageEnhancer {
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut updated = Image::new();
-        let mut pixel = self.image.lowest_pos();
-        loop {
+        for pixel in OffsetRowMajorPositionIterator::new(self.image.min_col - BORDER,
+                                                         self.image.min_row - BORDER,
+                                                         self.image.max_col + BORDER,
+                                                         self.image.max_row + BORDER) {
             updated.set(pixel.col, pixel.row, self.algorithm.is_set(self.image.neighborhood(pixel)));
-            match self.image.next_pos(pixel) {
-                None => break,
-                Some(next) => {pixel = next;}
-            }
         }
         mem::swap(&mut self.image, &mut updated);
         Some(updated)
@@ -165,7 +176,7 @@ mod tests {
 
     #[test]
     fn test1() {
-        let algorithm = read_enhancement_algorithm(TEST_ALGORITHM);
+        //let algorithm = read_enhancement_algorithm(TEST_ALGORITHM);
         let image = read_image(&mut TEST_IMAGE.split_whitespace().map(|s| s.to_string()));
         assert_eq!(image.neighborhood(Position::from((2, 2))), 34);
     }
