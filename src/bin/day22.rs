5af1b48e5e162e558990ffba1df97ec8b2e36553
gjf2a <ferrer@hendrix.edu>
2021-12-23 04:58:59 -0600

    Day 22: Parts 1 and 2 both work!

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -2,7 +2,7 @@ use std::cmp::{max, min};
 use std::fmt::{Display, Formatter};
 use std::io;
 use std::str::FromStr;
-use advent_code_lib::{advent_main, all_lines, make_inner_io_error, make_io_error};
+use advent_code_lib::{advent_main, all_lines, combinations_of, make_inner_io_error, make_io_error};
 use itertools::Itertools;
 
 const PART_1_MAX: isize = 50;
@@ -15,6 +15,7 @@ fn main() -> io::Result<()> {
         let actions = AllActions::from_file(args[1].as_str())?;
         let part1 = actions.part1();
         println!("Part 1: {}", part1.total_on());
+        println!("Part 2: {}", actions.total_on());
         Ok(())
     })
 }
@@ -56,18 +57,41 @@ impl AllActions {
     }
 
     fn total_on(&self) -> usize {
-        let mut total = 0;
-        for (i, action) in self.actions.iter().enumerate() {
-            assert_eq!(action.action, CubeState::On);
-            let mut tentative = action.region.num_cubes();
-            for j in 0..i {
-                if let Some(intersection) = action.region.intersection(&self.actions[j].region) {
-                    tentative -= intersection.num_cubes();
+        //println!("total_on");
+        let mut activated = Vec::new();
+        for action in self.actions.iter() {
+            activated = action.apply_to(&activated);
+            /*println!("activated");
+            for cube in activated.iter() {
+                println!("{}: {}", cube.num_cubes(), cube);
+            }*/
+        }
+        //println!("done");
+        activated.iter().map(|cuboid| cuboid.num_cubes()).sum()
+    }
+}
+
+impl CuboidAction {
+    fn apply_to(&self, activated: &Vec<Cuboid>) -> Vec<Cuboid> {
+        let mut result = Vec::new();
+        for cuboid in activated.iter() {
+            match self.region.intersection(cuboid) {
+                None => {result.push(cuboid.clone())}
+                Some(intersection) => {
+                    if let Some(broken_pieces) = cuboid.break_out(&intersection) {
+                        for piece in broken_pieces {
+                            if piece != intersection {
+                                result.push(piece);
+                            }
+                        }
+                    }
                 }
             }
-            total += tentative;
         }
-        total
+        if self.action == CubeState::On {
+            result.push(self.region.clone());
+        }
+        result
     }
 }
 
@@ -83,6 +107,35 @@ impl Cuboid {
 
     fn envelops(&self, other: &Cuboid) -> bool {
         self.ranges.iter().zip(other.ranges.iter()).all(|(a, b)| a.envelops(b))
+    }
+
+    fn break_out(&self, piece: &Cuboid) -> Option<Vec<Cuboid>> {
+        //println!("Breaking out piece: {} from self: {}", piece, self);
+        let break_outs = self.ranges.iter()
+            .zip(piece.ranges.iter())
+            .filter_map(|(a, b)| a.break_out(b))
+            .collect_vec();
+        //println!("break_outs:");
+        /*for break_out in break_outs.iter() {
+            println!("break_out: {:?}", break_out);
+        }*/
+        let num_break_outs = break_outs.iter().map(|v| v.len()).collect_vec();
+        if break_outs.len() == DIMENSIONS {
+            let mut result = Vec::new();
+            combinations_of(num_break_outs.iter().copied().max().unwrap(),
+                            &mut |indices: &[usize; DIMENSIONS]| {
+                                if indices.iter().zip(num_break_outs.iter()).all(|(i, len)| *i < *len) {
+                                    let mut ranges = [RangeDim { start: 0, end: 0 }; DIMENSIONS];
+                                    for i in 0..DIMENSIONS {
+                                        ranges[i] = break_outs[i][indices[i]];
+                                    }
+                                    result.push(Cuboid {ranges});
+                                }
+                            });
+            Some(result)
+        } else  {
+            None
+        }
     }
 
     fn intersection(&self, other: &Cuboid) -> Option<Self> {
@@ -115,6 +168,21 @@ impl RangeDim {
 
     fn envelops(&self, other: &RangeDim) -> bool {
         self.contains(other.start) && self.contains(other.end)
+    }
+
+    fn break_out(&self, piece: &RangeDim) -> Option<Vec<RangeDim>> {
+        self.intersection(piece)
+            //.filter(|intersection| *intersection != *self)
+            .map(|intersection| {
+                let mut result = vec![intersection];
+                if intersection.start > self.start {
+                    result.push(RangeDim::from(self.start, intersection.start - 1).unwrap());
+                }
+                if intersection.end < self.end {
+                    result.push(RangeDim::from(intersection.end + 1, self.end).unwrap());
+                }
+                result
+            })
     }
 
     fn intersection(&self, other: &RangeDim) -> Option<RangeDim> {
@@ -202,17 +270,47 @@ impl Display for AllActions {
 mod tests {
     use super::*;
 
+    const CUBOIDS_1: [&'static str; 2] = ["x=10..12,y=10..12,z=10..12", "x=11..13,y=11..13,z=11..13"];
+
     #[test]
     fn test_simple_count() {
-        for cuboid in ["x=10..12,y=10..12,z=10..12", "x=11..13,y=11..13,z=11..13"] {
+        for cuboid in CUBOIDS_1.iter() {
             let cuboid: Cuboid = cuboid.parse().unwrap();
             assert_eq!(cuboid.num_cubes(), 27);
         }
     }
 
     #[test]
-    fn easy_count() {
-        let actions = AllActions::from_file("ex/day22c.txt").unwrap();
-        assert_eq!(actions.total_on(), 46);
+    fn test_examples() {
+        for (example, goal) in [("c", 46), ("a", 39), ("b", 590784)] {
+            let actions = AllActions::from_file(format!("ex/day22{}.txt", example).as_str()).unwrap().part1();
+            assert_eq!(actions.total_on(), goal);
+        }
+    }
+
+    #[test]
+    fn test_break_out_1() {
+        let cuboids = CUBOIDS_1.iter().map(|s| s.parse::<Cuboid>().unwrap()).collect_vec();
+        let intersection = cuboids[0].intersection(&cuboids[1]);
+        println!("Intersection: {}", intersection.unwrap());
+        let breaks = cuboids[0].break_out(&cuboids[1]).unwrap();
+        for broken in breaks.iter() {
+            println!("{}", broken);
+        }
+        let break_sum = breaks.iter().map(|b| b.num_cubes()).sum::<usize>();
+        assert_eq!(break_sum, cuboids[0].num_cubes());
+    }
+
+    #[test]
+    fn test_break_out_2() {
+        let one = "x=10..10,y=11..12,z=11..12".parse::<Cuboid>().unwrap();
+        let two = "x=9..11,y=9..11,z=9..11".parse::<Cuboid>().unwrap();
+        let intersection = one.intersection(&two).unwrap();
+        println!("Intersection: {}", intersection);
+        let broken = one.break_out(&two).unwrap();
+        println!("Broken out:");
+        for cuboid in broken {
+            println!("{}", cuboid);
+        }
     }
 }
